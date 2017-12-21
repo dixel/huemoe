186eb1a9fa53ec0541168824a761cdf97803b763
Vasiliy Avdiushkin <dixelt@gmail.com>
2017-12-21 23:39:26 +0100

    cleanup dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,6 @@
                  [ring/ring-core "1.6.3"]
                  [cheshire "5.8.0"]
                  [clj-http "3.7.0"]
-                 [org.apache.xmlrpc/xmlrpc-client "3.1.3"]
                  [mount "0.1.11"]
                  [environ "1.1.0"]
                  [com.taoensso/timbre "4.10.0"]
