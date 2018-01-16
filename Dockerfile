016c985b02ed653b2f4d2b9a6df67c6d2ee8dddc
Vasilii Avdiushkin <dixelt@gmail.com>
2018-01-16 02:04:25 +0100

    unify uberjar generation

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:8
-COPY target/huemoe-0.1.0-SNAPSHOT-standalone.jar /usr/lib/huemoe.jar
+COPY target/huemoe.jar /usr/lib/huemoe.jar
 CMD HUEMOE_ENV=env java -jar /usr/lib/huemoe.jar
--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main dixel.huemoe.core
+  :uberjar-name "huemoe.jar"
   :profiles {:uberjar
              {:aot :all}}
   :dependencies [[org.clojure/clojure "1.8.0"]
