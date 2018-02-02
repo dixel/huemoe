05bd74cff2fe3356fea0ddd29c562afd0b11575b
Vasiliy Avdiushkin <dixelt@gmail.com>
2018-02-02 23:27:50 +0100

    test turning off all the lights

--- test/dixel/huemoe/bot_test.clj
+++ test/dixel/huemoe/bot_test.clj
@@ -1,5 +1,6 @@
 (ns dixel.huemoe.bot-test
   (:require [dixel.huemoe.bot :as sut]
+            [dixel.huemoe.keyboards :as kbd]
             [clojure.core.async :refer [>! <! <!! >!! chan go go-loop] :as a]
             [clojure.test :refer [deftest is testing] :as t]
             [taoensso.timbre :as log]
@@ -51,3 +52,18 @@
                                   :chat {:id 1}
                                   :from {:username "but-not-really"}}})
         (is (= nil (get @text-atom 1)))))))
+
+(deftest all-lights-off-test
+  (let [lamps #{"1" "2" "3"}
+        lamps-state (atom {"1" 100
+                           "2" 98
+                           "3" 56})]
+    (with-redefs
+      [dixel.huemoe.hue/get-active-device-ids (fn [_] lamps)
+       dixel.huemoe.hue/set-brightness (fn [_ lamp bri]
+                                         (swap! lamps-state
+                                                (fn [ls]
+                                                  (assoc ls lamp 0))))]
+      (sut/handle-generic-command 0 (kbd/command->button :all-off))
+      (doseq [[id bri] @lamps-state]
+        (is (= 0 bri))))))
